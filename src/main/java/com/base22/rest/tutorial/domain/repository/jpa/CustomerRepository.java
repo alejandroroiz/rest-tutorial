2f9ba04c7612e403868fcf7384d767900ec1d925
Alejandro Roiz <alejandro.roiz.walss@gmail.com>
2020-10-29 12:52:03 -0600

    fix comments

--- src/main/java/com/base22/rest/tutorial/domain/repository/jpa/CustomerRepository.java
+++ src/main/java/com/base22/rest/tutorial/domain/repository/jpa/CustomerRepository.java
@@ -2,12 +2,11 @@ package com.base22.rest.tutorial.domain.repository.jpa;
 
 import com.base22.rest.tutorial.domain.model.jpa.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called customerRepository
 // CRUD refers Create, Read, Update, Delete
 
-// The CrudRepository<> is taking the Class and the type of the primary key,
+// The JpaRepository<> is taking the Class and the type of the primary key,
 // so it knows which class its going to index and how
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     // Auto implemented functions:
--- src/main/java/com/base22/rest/tutorial/domain/service/CustomerService.java
+++ src/main/java/com/base22/rest/tutorial/domain/service/CustomerService.java
@@ -6,7 +6,6 @@ import com.base22.rest.tutorial.domain.repository.jpa.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
