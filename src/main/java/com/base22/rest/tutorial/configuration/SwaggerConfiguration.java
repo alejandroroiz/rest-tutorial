128da982bf244c06e8e9e27f29113067250bd72f
Diego Contreras <diego.contreras@base22.com>
2020-11-05 12:26:22 -0600

    Remove deprecated function

--- src/main/java/com/base22/rest/tutorial/configuration/SwaggerConfiguration.java
+++ src/main/java/com/base22/rest/tutorial/configuration/SwaggerConfiguration.java
@@ -17,7 +17,6 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -33,7 +32,8 @@ public class SwaggerConfiguration {
     logger.info("Registering Swagger2 documentation");
     Docket docket =
         new Docket(DocumentationType.SWAGGER_2).host("localhost:8080").select().apis(RequestHandlerSelectors.any())
-            .paths(Predicates.not(PathSelectors.regex("/error.*"))).build().securitySchemes(Arrays.asList(apiKey()))
+            .paths(Predicates.not(PathSelectors.regex("/error.*"))).build()
+            .securitySchemes(Collections.singletonList(apiKey()))
             .securityContexts(Collections.singletonList(securityContext())).apiInfo(apiInfo());
 
     logger.info("Swagger2 documentation registered correctly");
