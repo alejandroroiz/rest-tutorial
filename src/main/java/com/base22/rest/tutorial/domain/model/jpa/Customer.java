ec43bc8c178bf80cb974946e5673a2e3e348cd3e
Diego Contreras <diego.contreras@base22.com>
2020-11-06 19:51:09 -0600

    Add bad request error messages

--- src/main/java/com/base22/rest/tutorial/domain/model/jpa/Customer.java
+++ src/main/java/com/base22/rest/tutorial/domain/model/jpa/Customer.java
@@ -18,19 +18,19 @@ public class Customer {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @NotEmpty
+  @NotEmpty(message = "'name' cannot be null or empty")
   private String name;
 
-  @NotEmpty
-  @Email
-  @Column(unique = true )
+  @NotEmpty(message = "'email' cannot be null or empty")
+  @Email(message = "Must provide a valid email")
+  @Column(unique = true)
   private String email;
 
-  @NotEmpty
-  @Column(unique = true )
+  @NotEmpty(message = "'username' cannot be null or empty")
+  @Column(unique = true)
   private String username;
 
-  @NotEmpty
+  @NotEmpty(message = "'password' cannot be null or empty")
   private String password;
 
   public Customer(String name, String email, String username, String password) {
