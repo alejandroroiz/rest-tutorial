7f3dcf645698e247ab783e89485e8e9e6ed8e27d
Alejandro Roiz <alejandro.roiz.walss@gmail.com>
2021-01-11 15:27:29 -0600

    only run commandlinerunner on local environment

--- src/main/java/com/base22/rest/tutorial/Runner.java
+++ src/main/java/com/base22/rest/tutorial/Runner.java
@@ -20,7 +20,8 @@ public class Runner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         String environment = System.getenv(ENVIRONMENT_JVM_PROPERTY);
 
-        if (environment == "LOCAL") {
+        if (environment.startsWith("LOCAL")) {
+            logger.info("[Rest Tutorial] Running CommandLineRunner -- Creating dummy data");
             service.deleteAllCustomers();
 
             service.generate("Alex", "alex@test.com", "alejandro", "alex123");
