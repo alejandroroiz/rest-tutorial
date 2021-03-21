4fb0ca9d7f2fd0d456769040660c5399f19ddc3b
Diego Contreras <diego.contreras@base22.com>
2021-03-21 12:33:51 -0600

    Update DatabaseConfiguration to use getStringProperty method

--- src/main/java/com/base22/rest/tutorial/configuration/DatabaseConfiguration.java
+++ src/main/java/com/base22/rest/tutorial/configuration/DatabaseConfiguration.java
@@ -1,9 +1,6 @@
 package com.base22.rest.tutorial.configuration;
 
-import com.base22.rest.tutorial.exception.AppConfigException;
-import com.base22.rest.tutorial.util.AppConfigManager;
 import com.base22.rest.tutorial.util.ApplicationUtils;
-import org.apache.commons.configuration2.FileBasedConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -20,27 +17,18 @@ public class DatabaseConfiguration {
   @Bean
   public DataSource getDataSource() {
 
-    FileBasedConfiguration config;
+    logger.info("Starting initializing the database datasource");
 
     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-    try {
 
-      config = AppConfigManager.getConfiguration(ApplicationUtils.getConfigurationFile());
-      dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-      dataSourceBuilder.url(config.getString("database.url"));
-      dataSourceBuilder.username(config.getString("database.username"));
-      dataSourceBuilder.password(config.getString("database.password"));
-    } catch (AppConfigException e) {
-      logger.error("AppConfigException: ", e);
-      logger.error("There was an error configuring the database data source");
-      return null;
-    }
+    dataSourceBuilder.driverClassName(ApplicationUtils.getStringProperty("database.driver"));
+    dataSourceBuilder.url(ApplicationUtils.getStringProperty("database.url"));
+    dataSourceBuilder.username(ApplicationUtils.getStringProperty("database.username"));
+    dataSourceBuilder.password(ApplicationUtils.getStringProperty("database.password"));
+
+    logger.info("Successfully initialized the database datasource");
 
     return dataSourceBuilder.build();
   }
-
-  /*@Bean
-  public HibernateJpaAutoConfiguration getConfiguration() {
-    
-  }*/
+  
 }
